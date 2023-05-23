c2f736d78f6b761c6137e82c1337523f649a3141
asmarais <94301506+asmarais@users.noreply.github.com>
2023-05-23 17:45:46 +0100

    Update mainFunction.c

--- mainFunction.c
+++ mainFunction.c
@@ -1,4 +1,4 @@
-#include "SIMPLE_SHELL_H"
+#include "simple_shell.h"
 
 
 /**
@@ -50,4 +50,130 @@ char *_read(void)
         }
     }
     return (buffer);
+}
+
+/**
+* builtinFunction - check if the string is a builtin or not
+* @av: pointer to array of user of strings
+* @buffer: pointer to user string
+* @exitstatus: exit status of execve
+* Return: 1 if the string is equal env or 0 other wise  
+*/
+int builtinFunction(char **av, char *buffer, int exitstatus)
+{
+	int i;
+
+	if (_strcmp(av[0], "env") == 0)
+	{
+		_env();
+		for (i = 0; av[i]; i++)
+			free(av[i]);
+		free(av);
+		free(buffer);
+		return (1);
+	}
+	else if (_strcmp(av[0], "exit") == 0)
+	{
+		for (i = 0; av[i]; i++)
+			free(av[i]);
+		free(av);
+		free(buffer);
+		exit(exitstatus);
+	}
+	else
+		return (0);
+}
+
+/**
+ * _fork - create child process to execute the user input
+ * @av: pointer to array of strings
+ * @buffer: pointer to user string
+ * @fullpathbuffer: pointer to user input
+ *
+ * Return: 0 on success
+ */
+int _fork(char **av, char *buffer, char *fullpathbuffer)
+{
+	int i, status, result, exitstatus = 0;
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error");
+		exit(1);
+	}
+	if (pid == 0)
+	{
+		result =  execve(fullpathbuffer, av, environ);
+		if (result == -1)
+		{
+			perror(av[0]);
+			for (i = 0; av[i]; i++)
+				free(av[i]);
+			free(av);
+			free(buffer);
+			exit(127);
+		}
+	}
+	wait(&status);
+	if (WIFEXITED(status))
+	{
+		exitstatus = WEXITSTATUS(status);
+	}
+	for (i = 0; av[i]; i++)
+		free(av[i]);
+	free(av);
+	free(buffer);
+	return (exitstatus);
+}
+/**
+ * _fullpathbuffer - finds the string to call execve on
+ * @av: pointer to array of user strings
+ * @PATH: pointer to PATH string
+ * @copy: pointer to copy of PATH string
+ *
+ * Return: a pointer to string to call execve on
+ */
+
+char *_fullpathbuffer(char **av, char *PATH, char *copy)
+{
+	char *tok = NULL, *fullpathbuffer = NULL, *concatstr = NULL;
+	static char tmp[256];
+	int PATHcount = 0, fullpathflag = 0, z = 0, toklen = 0;
+	struct stat h;
+
+	copy = NULL;
+	copy = _strdup(PATH);
+	PATHcount = _splitPATH(copy);
+	tok = strtok(copy, ": =");
+	while (tok != NULL)
+	{
+		concatstr = _concat(tmp, av, tok);
+		if (stat(concatstr, &h) == 0)
+		{
+			fullpathbuffer = concatstr;
+			fullpathflag = 1;
+			break;
+		}
+		if (z < PATHcount - 2)
+		{
+			toklen = _strlen(tok);
+			if (tok[toklen + 1] == ':')
+			{
+				if (stat(av[0], &h) == 0)
+				{
+					fullpathbuffer = av[0];
+					fullpathflag = 1;
+					break;
+				}
+			}
+		}
+		z++;
+		tok = strtok(NULL, ":");
+	}
+	if (fullpathflag == 0)
+		fullpathbuffer = av[0];
+	free(copy);
+	return (fullpathbuffer);
 }
