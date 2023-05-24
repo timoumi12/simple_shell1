2ca4c9f41c2eb75bab4e0722a3b78bcb1f927827
timoumi12 <ihebtimoumi1@gmail.com>
2023-05-24 12:27:08 +0100

    fourth update

--- main.c
+++ main.c
@@ -1,13 +1,41 @@
 #include "simple_shell.h"
 
 /**
- * main - runs the shell
+ * main - runs the shell program
  *
- * Return: 0
-*/
-
+ * Return: 0 on success
+ */
 int main(void)
 {
-	
+	char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
+	char *PATH = NULL;
+	char **av = NULL;
+	int exitstatus = 0;
+
+	signal(SIGINT, SIG_IGN);
+	PATH = _getenviron("PATH");
+	if (PATH == NULL)
+		return (-1);
+	while (1)
+	{
+		av = NULL;
+		prompt();
+		buffer = _read();
+		if (*buffer != '\0')
+		{
+			av = splitbuffer(buffer);
+			if (av == NULL)
+			{
+				free(buffer);
+				continue;
+			}
+			fullpathbuffer = _fullpathbuffer(av, PATH, copy);
+			if (builtinFunction(av, buffer, exitstatus) != 0)
+				continue;
+			exitstatus = _fork(av, buffer, fullpathbuffer);
+		}
+		else
+			free(buffer);
+	}
 	return (0);
 }
