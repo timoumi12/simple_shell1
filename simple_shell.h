67ee11419b915740e5bedd222a7f94c823a437fe
asmarais <94301506+asmarais@users.noreply.github.com>
2023-05-24 12:12:33 +0100

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -32,6 +32,13 @@ int _strlen(char *s);
 
 /* PATH */
 
+char **_splitbuffer(char *buffer);
+int _splitPATH(char *str);
+int _PATHcompare(const char *str1, const char *str2);
+char *_concat(char *tmp, char **av, char *tok);
+
+/**/
+
 
 
 
